69e062d0f8871d5dc516f42411efab5b1d526297
10257514 <shejil.ka1@gmail.com>
2025-12-30 15:49:57 +0530

    integers

---
+++ integers/adder.go
@@ -0,0 +1,6 @@
+package integers
+
+//to add two number and return their sum
+func Add(x,y int) int {
+	return x+y
+}
---
+++ integers/adder_test.go
@@ -0,0 +1,13 @@
+package integers
+
+import (
+// "testing"
+"fmt"
+)
+
+func ExampleAdd(){
+	sum := Add(2,2)
+	// get := 4
+fmt.Println(sum)
+	// Output: 4
+}
