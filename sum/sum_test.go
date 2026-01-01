7b470db2916cbb6da01fb08689d8b31cf9d6c214
10257514 <shejil.ka1@gmail.com>
2026-01-01 12:06:56 +0530

    sum using array and slices

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -1,13 +1,34 @@
 package sum
 
-import ("testing")
+import (
+	"testing"
+)
 
 func TestSum(t *testing.T){
-	numbers := [5]int{2,3,5,1,1}
-	sum := Sum(numbers)
-	expected := 12
-	if sum != expected {
-		t.Errorf("sum is %d, but expecteted is %d",sum,expected)
+	// numbers := [5]int{2,3,5,1,1}
+	// sum := Sum(numbers)
+	// expected := 12
+	// if sum != expected {
+	// 	t.Errorf("sum is %d, but expecteted is %d",sum,expected)
 
-	}
+	// }
+	t.Run("for fixed numbers sum",func(t *testing.T){
+			numbers := []int{2,3,5,1,1}
+			sum := Sum(numbers)
+			expected := 12
+			if sum != expected {
+				t.Errorf("sum is %d, but expecteted is %d",sum,expected)
+
+			}
+	})
+
+	t.Run("for any numbers sum",func (t *testing.T)  {
+		numbers := []int{3,5,8,4}
+		sum := Sum(numbers)
+			expected := 20
+			if sum != expected {
+				t.Errorf("sum is %d, but expecteted is %d",sum,expected)
+
+			}
+	})
 }
